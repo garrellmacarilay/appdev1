4f622b37884c6e227cb17ca95eced747fb9e550e
GarrellMac <gmacarilay1@gmail.com>
2025-09-06 12:55:29 +0800

    Add modules export activity

---
+++ 11_javascript-refresher/15_modules_export.js
@@ -0,0 +1,15 @@
+// Create a file that exports:
+// A default function that returns a greeting.
+// A named constant object containing your information (e.g., name and age).
+// (No starter code for this one – create from scratch.)
+
+
+export default function greet() {
+  return "Hello! Welcome to my module.";
+}
+
+
+export const myInfo = {
+  name: "Garrell Macarilay",
+  age: 21
+};
