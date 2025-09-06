9454e432d2a7bedebb0fc85095e328632f29ceab
GarrellMac <gmacarilay1@gmail.com>
2025-09-06 12:35:19 +0800

    Add let and const activity

---
+++ 11_javascript-refresher/10_let_const.js
@@ -0,0 +1,26 @@
+// Use let and const to declare variables.
+// Try reassigning a variable declared with let and one with const.
+// Observe the difference between them and compare with var.
+
+const myInstructor = "Mr. Elmer Alvarado"; 
+let instructorAge = 33; 
+
+// myInstructor = "Mr. Jehu Casimiro"; - Meron error dito kasi const variable cannot be reassigned
+instructorAge = 24;
+
+// Update this code and replace the value with your name and age
+var oldName = "Elmer"; 
+var oldAge = 30;
+oldAge = 20; 
+
+
+let myName = "Garrell";
+let myAge = 21;
+
+
+console.log(myInstructor);   
+console.log(instructorAge);  
+console.log(oldName);        
+console.log(oldAge);         
+console.log(myName);        
+console.log(myAge);          
