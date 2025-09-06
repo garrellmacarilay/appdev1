5ce2a2c5879cbfb70c1b98689848419329ff3f78
GarrellMac <gmacarilay1@gmail.com>
2025-09-06 13:39:43 +0800

    Add let and const activity

--- 11_javascript-refresher/11_arrow_functions.js
+++ 11_javascript-refresher/11_arrow_functions.js
@@ -1,21 +1,22 @@
 // Convert the following traditional functions into arrow functions.
 // Include examples with no parameters, one parameter, and implicit return.
 
-function greet(name) {
-  return "Hello, " + name;
-}
+// function greet(name) {
+//   return "Hello, " + name;
+// }
 
 const greetArrow = (name) => "Hello, " + name;
-const add = (a, b) => a + b;
+console.log(greetArrow("Garrell")); 
 
-function square(n) {
-  return n * n;
-}
+// function square(n) {
+//   return n * n;
+// }
 const squareArrow = n => n * n;
+console.log(squareArrow(4));
 
 
-function sayHi() {
-  console.log("Hi!");
-}
-
+// function sayHi() {
+//   console.log("Hi!");
+// }
 const sayHiArrow = () => console.log("Hi!");
+sayHiArrow();
