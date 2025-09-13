d604bcc62a645689ed10e8d54680cd6b0668debd
GarrellMac <gmacarilay1@gmail.com>
2025-09-13 10:48:17 +0800

    Displaying data from the object

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -3,7 +3,6 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-
 function Button() {
   return (
     <button>About Me</button>
@@ -11,11 +10,22 @@ function Button() {
 }
 
 function App() {
- 
+  const myInfo = {
+  name: "Garrell Macarilay",
+  age: 21,
+  hobbies: ["Coding", "Cycling", "Sleeping"],
+  quote:"Triple D: Diligent, Determined, Dedicated"
+}
   return (
     <>
     <h1 className='title'>WELCOME TO MY PORFOLIO</h1>
-      <Button/>
+    <Button/>
+    <div>
+      <span>{`My name is ${myInfo.name}`}</span>
+      <span>{`I am ${myInfo.age} years old`}</span>
+      <span>{`My favorite hobby is ${myInfo.hobbies[0]}`}</span>
+    </div>
+
     </>
   )
 }
