eef7ec0978c3f6f9f2acfc1093baeefbb1ce3d79
GarrellMac <gmacarilay1@gmail.com>
2025-10-24 00:32:53 +0800

    Done with how to write your first React component

---
+++ react-describing-ui/src/components/Profile.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Profile() {
+  return (
+    <>
+         <img
+            className="w-16 h-16 sm:w-20 sm:h-20 rounded-full object-cover border-2 border-white shadow-md"
+            src="https://i.imgur.com/MK3eW3As.jpg"
+            alt="Katherine Johnson"
+         />
+    </>
+  )
+}
+
+export default Profile
