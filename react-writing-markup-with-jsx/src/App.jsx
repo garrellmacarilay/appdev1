e39d0d64d87d3d91e3942decd8917910688dce21
GarrellMac <gmacarilay1@gmail.com>
2025-11-09 21:41:31 +0800

    Done with the rules of JSX

--- react-writing-markup-with-jsx/src/App.jsx
+++ react-writing-markup-with-jsx/src/App.jsx
@@ -14,22 +14,13 @@ function App()
       //       <li>Rehearse a movie scene
       //       <li>Improve the spectrum technology
       //   </ul>
+    // Use React Fragment to wrap multiple elements
     <>
-      {/* <h1>Hedy Lamarr's Todos</h1>
-      <img 
-        src="https://i.imgur.com/yXOvdOSs.jpg" 
-        alt="Hedy Lamarr" 
-        className="photo" 
-      />
-      <ul>
-        <li>Invent new traffic lights</li>
-        <li>Rehearse a movie scene</li>
-        <li>Improve the spectrum technology</li>
-      </ul> */}
       <h1>Hedy Lamarr's Todos</h1>
       <img 
         src="https://i.imgur.com/yXOvdOSs.jpg" 
         alt="Hedy Lamarr" 
+        // Use camelCase for class attribute
         className="photo"
         // Use Closed Tag
       />
