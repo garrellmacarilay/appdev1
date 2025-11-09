a94f535780c1048edb76b03bee49e4ebd63fcefd
GarrellMac <gmacarilay1@gmail.com>
2025-11-09 19:47:19 +0800

    Done with the root component file

---
+++ react-importing-exporting-components/src/Gallery.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Profile() {
+  return (
+    <img
+      src="https://i.imgur.com/QIrZWGIs.jpg"
+      alt="Alan L. Hart"
+    />
+  );
+}
+
+
+
+export default function Gallery() {
+  return (
+    <div>Gallery</div>
+  )
+}
+
