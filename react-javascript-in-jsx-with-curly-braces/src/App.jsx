66800098b03accec10d8c1ca4e41180ae5d746a1
GarrellMac <gmacarilay1@gmail.com>
2025-11-22 10:29:14 +0800

    Done with more fun with JavaScript objects and curly braces

--- react-javascript-in-jsx-with-curly-braces/src/App.jsx
+++ react-javascript-in-jsx-with-curly-braces/src/App.jsx
@@ -20,7 +20,6 @@ function App() {
   return (
     <>
       <TodoList />
-      <Avatar />
     </>
   )
 }
--- react-javascript-in-jsx-with-curly-braces/src/TodoList.jsx
+++ react-javascript-in-jsx-with-curly-braces/src/TodoList.jsx
@@ -1,17 +1,27 @@
 export default function TodoList() {
-  const name = 'Gregorio Y. Zara';
+  const person = {
+    name: 'Gregorio Y. Zara',
+    theme: {
+        backgroundColor: 'black',
+        color: 'pink'
+    }
+ };
+
   return (
     <>
-        <h1>{name}'s To Do List</h1>
-        <ul style={{
-            backgroundColor: 'black',
-            color: 'pink'
-        }}>
-            <li>Improve the videophone</li>
-            <li>Prepare aeronautics lectures</li>
-            <li>Work on the alcohol-fuelled engine</li>
-        </ul>
-
+     <div style={person.theme}>
+      <h1>{person.name}'s Todos</h1>
+      <img
+        className="avatar"
+        src="https://i.imgur.com/7vQD0fPs.jpg"
+        alt="Gregorio Y. Zara"
+      />
+      <ul>
+        <li>Improve the videophone</li>
+        <li>Prepare aeronautics lectures</li>
+        <li>Work on the alcohol-fuelled engine</li>
+      </ul>
+    </div>
     </>
     
   );
