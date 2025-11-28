a85b3126ba6e5a40e58faffa0cc913903744b0ee
GarrellMac <gmacarilay1@gmail.com>
2025-11-28 21:26:45 +0800

    Done with Create Todo Components

---
+++ react-todo-app-useeffect-router-redux/src/components/TodoList.jsx
@@ -0,0 +1,17 @@
+
+import TodoItem from "./TodoItem";
+
+
+function TodoList({todos}) {
+  return (
+    <>
+         <ul>
+            {todos.map((todo) => (
+                <TodoItem key={todo.id} todo={todo} />
+            ))}
+        </ul>
+    </>
+  )
+}
+
+export default TodoList
