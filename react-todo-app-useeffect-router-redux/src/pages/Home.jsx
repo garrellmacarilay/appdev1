a28a4761f4656638bcce152eaa801107508dfdcd
GarrellMac <gmacarilay1@gmail.com>
2025-11-28 17:23:08 +0800

    Done with Create the Home page

---
+++ react-todo-app-useeffect-router-redux/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <>
+       <h1>Welcome to the Todo App</h1>
+       <a href="/login">Login</a>
+    </>
+   
+  )
+}
+
+export default Home
